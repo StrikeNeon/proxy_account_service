fa6147717a86e03f7a27ea0e11cf117b6e92c6b6
Tony <SjasFaceMD@gmail.com>
2021-06-22 20:15:03 +0300

    adding swagger

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from . import app
 from flask_restful import Api
+from flask_swagger_ui import get_swaggerui_blueprint
 from .router import add_routes
 
 api = Api(app)
 add_routes(api)
+
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Proxy Account Service"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+
 app.run(debug=True)
--- resources.py
+++ resources.py
@@ -10,8 +10,8 @@ from .schema import (account_schema, accounts_schema,
 from .models import Account, Lock, Proxy, Net_Resource
 
 
-class GP_maker():
-    # this only works on something easy to parse
+class general_request_processor():
+
     def __init__(self, model, schema, request_json):
         self.model = model
         self.schema = schema
@@ -19,7 +19,7 @@ class GP_maker():
 
     def make_record(self):
         # this isn't the best way here, can't think of anything else
-        non_nullable = [col.name for col in self.model.__table__.columns if not col.nullable]
+        non_nullable = [col.name for col in self.model.__table__.columns if not col.nullable and col.name != "id"]
         if len(non_nullable) == len([key for key in self.json_data.keys() if key in non_nullable]):
             additional = [col.name for col in self.model.__table__.columns if col.nullable]
             record_dict = {key: value for key, value
@@ -39,6 +39,67 @@ class GP_maker():
         else:
             return {"error": "one or more non nullable fields was not supplied"}, 400, {'content-type': 'application/json'}
 
+    def update_record(self, record_id: str):
+        table_columns = [col.name for col in self.model.__table__.columns]  # get model column names
+        update_dict = {key: value for key, value
+                       in self.json_data.items() if key in table_columns}  # parse request json to only get necessary data
+        queried_record = self.model.query.filter_by(id=record_id)
+        queried_record.update(update_dict)
+        db.session.commit()
+        return self.schema.dump(queried_record)
+
+    def make_lock(self, requested_id: str, many: bool = False):
+        if request.json.get("expires_at", None):
+            if self.model == Account:
+                check_lock = Lock.query.filter_by(locked_account=requested_id)
+                if len(check_lock) > 0:
+                    if many:
+                        return check_lock
+                    return {"error": f"account {requested_id} already locked"}, 201, {'content-type': 'application/json'}
+                new_lock = Lock(
+                            locked_account=self.json_data.get(requested_id),
+                            locked_at=datetime.now(),
+                            expires_at=datetime.fromtimestamp(self.json_data.get('expires_at'))
+                            )
+                db.session.add(new_lock)
+                db.session.commit()
+                if many:
+                    return new_lock
+                return lock_schema.dump(new_lock)
+            else:
+                check_lock = Lock.query.filter_by(locked_proxy=requested_id)
+                if len(check_lock) > 0:
+                    if many:
+                        return check_lock
+                    return {"error": f"proxy {requested_id} already locked at {check_lock.id}"}, 201, {'content-type': 'application/json'}
+                new_lock = Lock(locked_proxy=self.json_data.get(requested_id),
+                                locked_at=datetime.now(),
+                                expires_at=datetime.fromtimestamp(self.json_data.get('expires_at'))
+                                )
+                db.session.add(new_lock)
+                db.session.commit()
+                if many:
+                    return new_lock
+                return lock_schema.dump(new_lock)
+        else:
+            return {"error": "No expiry provided"}, 400, {'content-type': 'application/json'}
+
+    def unmake_lock(self, requested_id: str, many: bool = False):
+        if self.model == Account:
+            deleted_lock = Lock.query.filter_by(locked_account=requested_id)
+        else:
+            deleted_lock = Lock.query.filter_by(locked_proxy=requested_id)
+        if deleted_lock:
+            deleted_lock.delete()
+            db.session.commit()
+            if many:
+                return deleted_lock
+            return lock_schema.dump(deleted_lock)
+        else:
+            if many:
+                return
+            return {"error": "wasn't locked"}, 201, {'content-type': 'application/json'}
+
 
 class Init_page(Resource):
 
@@ -49,7 +110,7 @@ class Init_page(Resource):
 class Account_maker(Resource):
 
     def post(self):
-        maker = GP_maker(Account, account_schema, request_json)
+        maker = general_request_processor(Account, account_schema, request.json)
         new_account = maker.make_record()
         return new_account
 
@@ -70,31 +131,23 @@ class Multiple_account_ops(Resource):
         if action == "lock":
             locked_accounts = []
             for account_id in accounts:
-                if account_id.get("expires_at", None):
-                    new_lock = Lock(
-                            locked_account=account_id.get('account_id'),
-                            locked_at=datetime.now(),
-                            expires_at=datetime.fromtimestamp(account_id.get('expires_at'))
-                        )
-                    db.session.add(new_lock)
-                    db.session.commit()
+                maker = general_request_processor(Account, account_schema, account_id)
+                new_lock = maker.make_lock(account_id.get('account_id'), many=True)
+                if new_lock:
                     locked_accounts.append(new_lock)
-                else:
-                    print({"error": "No expiry provided",
-                           "account": account_id})
             return {"locked_accounts": locks_schema.dump(locked_accounts)}
         elif action == "unlock":
-            locked_accounts = []
+            unlocked_accounts = []
             for account_id in accounts:
-                deleted_lock = Lock.query.filter_by(locked_account=account_id.get('account_id'))
-                deleted_lock.delete()
-                db.session.commit()
-                locked_accounts.append(new_lock)
-            return {"unlocked_accounts": locks_schema.dump(locked_accounts)}
+                maker = general_request_processor(Account, account_schema, account_id)
+                deleted_lock_lock = maker.unmake_lock(account_id.get('account_id'), many=True)
+                if new_lock:
+                    unlocked_accounts.append(deleted_lock_lock)
+            return {"unlocked_accounts": locks_schema.dump(unlocked_accounts)}
         else:
             return {"error": "action missing or unknown"}
 
-    def delete_account(self):
+    def delete(self):
         accounts = request.json.get('accounts', None)
         if not accounts or type(accounts) != list:
             return {"error": "account ids were not supplied or key malformed"}
@@ -114,35 +167,18 @@ class Account_ops(Resource):
         return account_schema.dump(account)
 
     def put(self, account_id):
-        table_columns = [col.name for col in Account.__table__.columns]  # get model column names
-        update_dict = {key: value for key, value
-                       in request.json.items() if key in table_columns}  # parse request json to only get necessary data
-        queried_account = Account.query.filter_by(id=account_id)
-        queried_account.update(update_dict)
-        db.session.commit()
-        return account_schema.dump(queried_account)
+        updater = general_request_processor(Account, account_schema, request.json)
+        updated_record = updater.update_record(account_id)
+        return updated_record
 
     def patch(self, account_id):
+        locker = general_request_processor(Account, account_schema, request.json)
         action = request.args.get('action')
         if action == "lock":
-            if request.json.get("expires_at", None):
-                new_lock = Lock(
-                        locked_account=request.json.get('account_id'),
-                        locked_at=datetime.now(),
-                        expires_at=datetime.fromtimestamp(request.json.get('expires_at'))
-                    )
-                db.session.add(new_lock)
-                db.session.commit()
-                return lock_schema.dump(new_lock)
-            else:
-                return {"error": "No expiry provided"}, 400, {'content-type': 'application/json'}
-        elif action == "unlock":
-            deleted_lock = Lock.query.filter_by(locked_account=account_id)
-            deleted_lock.delete()
-            db.session.commit()
-            return lock_schema.dump(deleted_lock)
+            new_lock = locker.make_lock(account_id)
+        return new_lock
 
-    def delete_account(account_id):
+    def delete(account_id):
         deleted_account = Account.query.filter_by(id=account_id)
         deleted_account.delete()
         db.session.commit()
@@ -152,7 +188,7 @@ class Account_ops(Resource):
 class Proxy_maker(Resource):
 
     def post(self):
-        maker = GP_maker(Proxy, proxy_schema, request.json)
+        maker = general_request_processor(Proxy, proxy_schema, request.json)
         new_proxy = maker.make_record()
         return new_proxy
 
@@ -168,36 +204,28 @@ class Multiple_proxy_ops(Resource):
     def patch(self):
         proxies = request.json.get('proxies', None)
         if not proxies or type(proxies) != list:
-            return {"error": "proxy ids were not supplied or key malformed"}
+            return {"error": "account ids were not supplied or key malformed"}
         action = request.args.get('action')
         if action == "lock":
             locked_proxies = []
             for proxy_id in proxies:
-                if proxy_id.get("expires_at", None):
-                    new_lock = Lock(
-                            locked_proxy=proxy_id.get('proxy_id'),
-                            locked_at=datetime.now(),
-                            expires_at=datetime.fromtimestamp(proxy_id.get('expires_at'))
-                        )
-                    db.session.add(new_lock)
-                    db.session.commit()
+                maker = general_request_processor(Proxy, proxy_schema, proxy_id)
+                new_lock = maker.make_lock(proxy_id.get('proxy_id'), many=True)
+                if new_lock:
                     locked_proxies.append(new_lock)
-                else:
-                    print({"error": "No expiry provided",
-                           "proxy": proxy_id})
             return {"locked_proxies": locks_schema.dump(locked_proxies)}
         elif action == "unlock":
-            locked_proxies = []
+            unlocked_proxies = []
             for proxy_id in proxies:
-                deleted_lock = Lock.query.filter_by(locked_proxy=proxy_id.get('proxy_id'))
-                deleted_lock.delete()
-                db.session.commit()
-                locked_proxies.append(new_lock)
-            return {"unlocked_proxies": locks_schema.dump(locked_proxies)}
+                maker = general_request_processor(Proxy, proxy_schema, proxy_id)
+                deleted_lock_lock = maker.unmake_lock(proxy_id.get('proxy_id'), many=True)
+                if new_lock:
+                    unlocked_proxies.append(deleted_lock_lock)
+            return {"unlocked_proxies": locks_schema.dump(unlocked_proxies)}
         else:
-            return {"error": "action missing or unknown"}, 400, {'content-type': 'application/json'}
+            return {"error": "action missing or unknown"}
 
-    def delete_account(self):
+    def delete(self):
         proxies = request.json.get('accounts', None)
         if not proxies or type(proxies) != list:
             return {"error": "proxy ids were not supplied or key malformed"}, 400, {'content-type': 'application/json'}
@@ -217,35 +245,16 @@ class Proxy_ops(Resource):
         return proxy_schema.dump(proxy)
 
     def put(self, proxy_id):
-        table_columns = [col.name for col in Proxy.__table__.columns]
-        update_dict = {key: value for key, value
-                       in request.json.items() if key in table_columns}
-        queried_proxy = Proxy.query.filter_by(id=proxy_id)
-        queried_proxy.update(update_dict)
-        db.session.commit()
-        return proxy_schema.dump(queried_proxy)
+        updater = general_request_processor(Proxy, proxy_schema, request.json)
+        updated_record = updater.update_record(proxy_id)
+        return updated_record
 
     def patch(self, proxy_id):
-        action = request.args.get('action')
-        if action == "lock":
-            if request.json.get("expires_at", None):
-                new_lock = Lock(
-                        locked_proxy=proxy_id,
-                        locked_at=datetime.now(),
-                        expires_at=datetime.fromtimestamp(request.json.get('expires_at'))
-                    )
-                db.session.add(new_lock)
-                db.session.commit()
-                return lock_schema.dump(new_lock)
-            else:
-                return {"error": "No expiry provided"}
-        elif action == "unlock":
-            deleted_lock = Lock.query.filter_by(locked_proxy=proxy_id)
-            deleted_lock.delete()
-            db.session.commit()
-            return lock_schema.dump(deleted_lock)
+        locker = general_request_processor(Proxy, proxy_schema, request.json)
+        new_lock = locker.make_lock(proxy_id, request.args)
+        return new_lock
 
-    def delete_account(proxy_id):
+    def delete(proxy_id):
         deleted_proxy = Proxy.query.filter_by(id=proxy_id)
         deleted_proxy.delete()
         db.session.commit()
@@ -264,7 +273,7 @@ class Multiple_resource_ops(Resource):
 class Resource_maker(Resource):
 
     def post(self):
-        maker = GP_maker(Net_Resource, resource_schema, request.json)
+        maker = general_request_processor(Net_Resource, resource_schema, request.json)
         new_resource = maker.make_record()
         return new_resource
 
@@ -276,13 +285,9 @@ class Resorce_ops(Resource):
         return resource_schema.dump(resource)
 
     def put(self, resource_id):
-        table_columns = [col.name for col in Net_Resource.__table__.columns]
-        update_dict = {key: value for key, value
-                       in request.json.items() if key in table_columns}
-        queried_resource = Net_Resource.query.filter_by(id=resource_id)
-        queried_resource.update(update_dict)
-        db.session.commit()
-        return resource_schema.dump(queried_resource)
+        updater = general_request_processor(Net_Resource, resource_schema, request.json)
+        updated_record = updater.update_record(resource_id)
+        return updated_record
 
     def delete(self, resource_id):
         deleted_resource = Net_Resource.query.filter_by(id=resource_id)
@@ -296,7 +301,7 @@ class Interlock(Resource):
     def post(self, lock_id):
         json_data = request.json
         json_data["locked_at"] = datetime.now()
-        maker = GP_maker(Lock, lock_schema, request.json)
+        maker = general_request_processor(Lock, lock_schema, request.json)
         new_lock = maker.make_record()
         return new_lock
 
@@ -309,13 +314,9 @@ class Lock_ops(Resource):
             return lock_schema.dump(account)
 
     def put(self, lock_id):
-        table_columns = [col.name for col in Lock.__table__.columns]
-        update_dict = {key: value for key, value
-                       in request.json.items() if key in table_columns}
-        queried_lock = Lock.query.filter_by(id=lock_id)
-        queried_lock.update(update_dict)
-        db.session.commit()
-        return lock_schema.dump(queried_lock)
+        updater = general_request_processor(Lock, lock_schema, request.json)
+        updated_record = updater.update_record(lock_id)
+        return updated_record
 
     def delete(self, lock_id):
         deleted_lock = Lock.query.filter_by(id=lock_id)
